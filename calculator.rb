88911486f79a8483d9e5709cd588951402e750a1
afleisch <afleisch@gmail.com>
2014-02-06 19:03:00 -0800

    changes

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,18 @@
 #hello this is a change
-puts "Enter a number, a + or - sign, then another number."
+puts "Enter a number, a +, -, /, or * sign, then another number."
 
-first_number = gets.to_i
+first_number = gets.to_f
 symbol = gets.chomp
-second_number = gets.to_i
+second_number = gets.to_f
 
 if symbol == "-"
 puts "Result: #{first_number - second_number}"
 elsif symbol == "+"
 puts "Result: #{first_number + second_number}"
+elsif symbol == "/"
+puts "Result: #{first_number / second_number}"
+elsif symbol == "*"
+puts "Result: #{first_number * second_number}"
 else
 	puts "Please re-read the instructions."
 end
